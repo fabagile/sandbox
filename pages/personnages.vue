1fd68892c858f93e0b7473528924430374b57250
fabagile <fabagile@outlook.fr>
2025-07-14 17:04:08 +0200

    maj rendu personnages

--- pages/personnages.vue
+++ pages/personnages.vue
@@ -4,12 +4,26 @@
 <template>
   <!-- <Title title="Personnages" /> -->
   <section>
-    <!-- <pre>
+    <!-- 
+    <pre>
       {{ data }}
-    </pre> -->
-    
+    </pre>
     <ul>
-      <li v-for="(character,i) in data" :k="i">{{ character.fname }}</li>
+      <li v-for="(character,i) in data" :k="i">
+        
+        {{ character.fname }}
+      </li>
     </ul>
+    -->
+    <dl>
+      <template v-for="(character,i) in data" :k="i">
+        <dt  >{{ character.fname }} {{ character.lname.toUpperCase() }}</dt>
+        <dd>{{ character.powers.join(", ") }}</dd>
+        
+      </template>
+    </dl>
+    
+    
+  
   </section>
 </template>
